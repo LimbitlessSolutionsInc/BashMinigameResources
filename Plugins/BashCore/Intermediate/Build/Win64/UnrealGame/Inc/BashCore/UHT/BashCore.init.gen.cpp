d3819f52c73a4fc5c0d6fbf8dc6d7b3bd7bebf08
mob205 <mobslayer205@gmail.com>
2025-06-11 12:21:49 -0400

    Update BashCore

--- Plugins/BashCore/Intermediate/Build/Win64/UnrealGame/Inc/BashCore/UHT/BashCore.init.gen.cpp
+++ Plugins/BashCore/Intermediate/Build/Win64/UnrealGame/Inc/BashCore/UHT/BashCore.init.gen.cpp
@@ -23,7 +23,7 @@ void EmptyLinkFunctionForGeneratedCodeBashCore_init() {}
 				SingletonFuncArray,
 				UE_ARRAY_COUNT(SingletonFuncArray),
 				PKG_CompiledIn | 0x00000000,
-				0x0926CF00,
+				0x54596941,
 				0xDEDC8374,
 				METADATA_PARAMS(nullptr, 0)
 			};
@@ -31,5 +31,5 @@ void EmptyLinkFunctionForGeneratedCodeBashCore_init() {}
 		}
 		return Z_Registration_Info_UPackage__Script_BashCore.OuterSingleton;
 	}
-	static FRegisterCompiledInInfo Z_CompiledInDeferPackage_UPackage__Script_BashCore(Z_Construct_UPackage__Script_BashCore, TEXT("/Script/BashCore"), Z_Registration_Info_UPackage__Script_BashCore, CONSTRUCT_RELOAD_VERSION_INFO(FPackageReloadVersionInfo, 0x0926CF00, 0xDEDC8374));
+	static FRegisterCompiledInInfo Z_CompiledInDeferPackage_UPackage__Script_BashCore(Z_Construct_UPackage__Script_BashCore, TEXT("/Script/BashCore"), Z_Registration_Info_UPackage__Script_BashCore, CONSTRUCT_RELOAD_VERSION_INFO(FPackageReloadVersionInfo, 0x54596941, 0xDEDC8374));
 PRAGMA_ENABLE_DEPRECATION_WARNINGS
